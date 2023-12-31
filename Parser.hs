e90c8cf4d5e1541b3f388ebad4cc4d371fa59ed9
diogotvf7 <diogotvf7@gmail.com>
2023-12-31 14:34:12 +0000

    parseB and function for getting body between parenthesis

--- Parser.hs
+++ Parser.hs
@@ -20,8 +20,6 @@ data Bexp
     | B_and Bexp Bexp        -- Logical AND
     deriving (Eq, Show)
 
-data Exp = Aexp | Bexp
-
 data Stm
     = S_assign String Aexp   -- Assignment statement
     | S_if Bexp [Stm] [Stm]  -- If statement with true and false branches
@@ -49,14 +47,28 @@ parseStatements (T_var var : T_assign : tokens) =
 --     in
 --         S_while bexp body : 
 
--- parseStatements (T_if _ tokens) = 
---     ...
+parseStatements (T_if : tokens) = 
+    let 
+        (bexp, tokens') = getBody tokens
+        ()
+
+getBody :: [Token] -> ([Token], [Token])
+getBody (T_lbracket : tokens) = getBodyAux tokens [] [T_lbracket]
+getBody tokens = (takeWhile (/= T_semicolon), drop 1 (dropWhile (/= T_semicolon) tokens))
+
+getBodyAux :: [Token] -> [Token] -> [Token] -> ([Token], [Token])
+getBodyAux [] body stack = error "Run-time error"
+getBodyAux (T_lbracket : tokens) body stack = getBodyAux tokens (body ++ [T_lbracket]) (T_lbracket : stack)
+getBodyAux (T_rbracket : tokens) body (T_lbracket : stack) = 
+    if stack == [] then (body, tokens)
+    else getBodyAux tokens (body ++ [T_rbracket]) stack
+getBodyAux (token : tokens) body stack = getBodyAux tokens (body ++ [token]) stack
 
 parseA :: [Token] -> Aexp
 parseA tokens =
-    case parseSumOrSubOrProdOrIntOrVarOrPar tokens of
+    case parseSumOrSubOrProdOrPar tokens of
         Just (expr, []) -> expr
-        _ -> error "Parse error"
+        _ -> error "Run-time error"
 
 -- Int Var Par
 parseIntOrVarOrPar :: [Token] -> Maybe (Aexp, [Token])
@@ -65,121 +77,113 @@ parseIntOrVarOrPar (T_var var : restTokens)
 parseIntOrVarOrPar (T_integer num : restTokens)
     = Just (A_num num, restTokens)
 parseIntOrVarOrPar (T_lbracket : restTokens1)
-    = case parseSumOrSubOrProdOrIntOrVarOrPar restTokens1 of
+    = case parseSumOrSubOrProdOrPar restTokens1 of
         Just (expr, (T_rbracket : restTokens2)) ->
             Just (expr, restTokens2)
         Just _ -> Nothing -- no closing paren
         Nothing -> Nothing
--- Reconheccer que voltamos a uma expressão booleana (????)
 parseIntOrVarOrPar tokens = Nothing
 
 -- Prod Int Var Par
-parseProdOrIntOrVarOrPar :: [Token] -> Maybe (Aexp, [Token])
-parseProdOrIntOrVarOrPar tokens
+parseProdOrPar :: [Token] -> Maybe (Aexp, [Token])
+parseProdOrPar tokens
     = case parseIntOrVarOrPar tokens of
         Just (expr1, (T_times : restTokens1)) ->
-            case parseProdOrIntOrVarOrPar restTokens1 of
+            case parseProdOrPar restTokens1 of
                 Just (expr2, restTokens2) ->
                     Just (A_mul expr1 expr2, restTokens2)
                 Nothing -> Nothing
         result -> result
 
 -- Sum Sub Prod Int Var Par
-parseSumOrSubOrProdOrIntOrVarOrPar::[Token] -> Maybe (Aexp, [Token])
-parseSumOrSubOrProdOrIntOrVarOrPar tokens
-    = case parseProdOrIntOrVarOrPar tokens of
+parseSumOrSubOrProdOrPar::[Token] -> Maybe (Aexp, [Token])
+parseSumOrSubOrProdOrPar tokens
+    = case parseProdOrPar tokens of
         Just (expr1, (T_plus : restTokens1)) ->
-            case parseSumOrSubOrProdOrIntOrVarOrPar restTokens1 of
+            case parseSumOrSubOrProdOrPar restTokens1 of
                 Just (expr2, restTokens2) ->
                     Just (A_add expr1 expr2, restTokens2)
                 Nothing -> Nothing
         Just (expr1, (T_less : restTokens1)) ->
-            case parseSumOrSubOrProdOrIntOrVarOrPar restTokens1 of
+            case parseSumOrSubOrProdOrPar restTokens1 of
                 Just (expr2, restTokens2) ->
                     Just (A_sub expr1 expr2, restTokens2)
                 Nothing -> Nothing
         result -> result
 
+-- ----------------------------------------------------------------------------
+
 parseB :: [Token] -> Bexp
 parseB tokens =
-    case parseAndOrBeqOrLeqOrAeqOrNotOrTrueOrFalseOrPar tokens of
+    case parseAndOrBeqOrNotOrAeqOrLeqOrTrueOrFalseOrPar tokens of
         Just (expr, []) -> expr
-        _ -> error "Parse error"
+        _ -> error "Run-time error"
 
--- True False ()
+-- True False Par
 parseTrueOrFalseOrPar :: [Token] -> Maybe (Bexp, [Token])
 parseTrueOrFalseOrPar (T_bool True : restTokens)
     = Just (B_true, restTokens)
 parseTrueOrFalseOrPar (T_bool False : restTokens)
     = Just (B_false, restTokens)
 parseTrueOrFalseOrPar (T_lbracket : restTokens1)
-    = case parseAndOrBeqOrLeqOrAeqOrNotOrTrueOrFalseOrPar restTokens1 of
+    = case parseAndOrBeqOrNotOrAeqOrLeqOrTrueOrFalseOrPar restTokens1 of
         Just (expr, (T_rbracket : restTokens2)) ->
             Just (expr, restTokens2)
         Just _ -> Nothing -- no closing paren
         Nothing -> Nothing
 parseTrueOrFalseOrPar tokens = Nothing
 
--- Not True False ()
-parseNotOrTrueOrFalseOrPar :: [Token] -> Maybe (Bexp, [Token])
-parseNotOrTrueOrFalseOrPar tokens
-    = case parseTrueOrFalseOrPar tokens of
+-- Leq True False Par
+parseLeqOrTrueOrFalseOrPar :: [Token] -> Maybe (Bexp, [Token])
+parseLeqOrTrueOrFalseOrPar tokens
+    = case parseSumOrSubOrProdOrPar tokens of
+        Just (expr1, (T_leq : restTokens1)) ->
+            case parseSumOrSubOrProdOrPar restTokens1 of
+                Just (expr2, restTokens2) ->
+                    Just (B_leq expr1 expr2, restTokens2)
+                Nothing -> Nothing
+        _ -> parseTrueOrFalseOrPar tokens
+
+-- Aeq Leq True False Par
+parseAeqOrLeqOrTrueOrFalseOrPar :: [Token] -> Maybe (Bexp, [Token])
+parseAeqOrLeqOrTrueOrFalseOrPar tokens
+    = case parseSumOrSubOrProdOrPar tokens of
+        Just (expr1, (T_leq : restTokens1)) ->
+            case parseSumOrSubOrProdOrPar restTokens1 of
+                Just (expr2, restTokens2) ->
+                    Just (B_leq expr1 expr2, restTokens2)
+                Nothing -> Nothing
+        _ -> parseLeqOrTrueOrFalseOrPar tokens
+
+-- Not Aeq Leq True False Par
+parseNotOrAeqOrLeqOrTrueOrFalseOrPar :: [Token] -> Maybe (Bexp, [Token])
+parseNotOrAeqOrLeqOrTrueOrFalseOrPar tokens
+    = case parseAeqOrLeqOrTrueOrFalseOrPar tokens of
         Just (expr, restTokens) -> Just (expr, restTokens)
         Nothing ->
-            case parseNotOrTrueOrFalseOrPar tokens of
+            case parseNotOrAeqOrLeqOrTrueOrFalseOrPar tokens of
                 Just (expr, (T_not : restTokens)) ->
                     Just (B_not expr, restTokens)
                 result -> result
 
--- Leq Aeq not True False ()
-parseLeqOrAeqOrNotOrTrueOrFalseOrPar :: [Token] -> Maybe (Bexp, [Token])
-parseLeqOrAeqOrNotOrTrueOrFalseOrPar tokens 
-    = case parseNotOrTrueOrFalseOrPar tokens of
-        Just (expr1, (T_leq : restTokens1)) ->
-            case parseLeqOrAeqOrNotOrTrueOrFalseOrPar restTokens1 of
-                Just (expr2, restTokens2) ->
-                    Just (B_leq expr1 expr2, restTokens2)
-                Nothing -> Nothing
-        Just (expr1, (T_aeq : restTokens1)) ->
-            case parseLeqOrAeqOrNotOrTrueOrFalseOrPar restTokens1 of
-                Just (expr2, restTokens2) ->
-                    Just (B_aeq expr1 expr2, restTokens2)
-                Nothing -> Nothing
-        result -> result
-
--- parseLeqOrAeqOrNotOrTrueOrFalseOrPar :: [Token] -> Maybe (Bexp, [Token])
--- parseLeqOrAeqOrNotOrTrueOrFalseOrPar tokens 
---     = case parseNotOrTrueOrFalseOrPar tokens of
---         Just (expr1, (T_aeq : restTokens1)) ->
---             case parseArithmeticExpr restTokens1 of
---                 Just (expr2, restTokens2) ->
---                     Just (B_aeq expr1 expr2, restTokens2)
---                 Nothing -> Nothing
---         Just (expr1, (T_leq : restTokens1)) ->
---             case parseArithmeticExpr restTokens1 of
---                 Just (expr2, restTokens2) ->
---                     Just (B_leq expr1 expr2, restTokens2)
---                 Nothing -> Nothing
---         result -> result
-
--- And Beq Leq Aeq not True False ()
-parseAndOrBeqOrLeqOrAeqOrNotOrTrueOrFalseOrPar :: [Token] -> Maybe (Bexp, [Token])
-parseAndOrBeqOrLeqOrAeqOrNotOrTrueOrFalseOrPar tokens
-    = case parseLeqOrAeqOrNotOrTrueOrFalseOrPar tokens of 
-        Just (expr1, (T_and : restTokens1)) ->
-            case parseAndOrBeqOrLeqOrAeqOrNotOrTrueOrFalseOrPar restTokens1 of
-                Just (expr2, restTokens2) ->
-                    Just (B_and expr1 expr2, restTokens2)
-                Nothing -> Nothing
+-- Beq Not Aeq Leq True False Par
+parseBeqOrNotOrAeqOrLeqOrTrueOrFalseOrPar :: [Token] -> Maybe (Bexp, [Token])
+parseBeqOrNotOrAeqOrLeqOrTrueOrFalseOrPar tokens
+    = case parseNotOrAeqOrLeqOrTrueOrFalseOrPar tokens of
         Just (expr1, (T_beq : restTokens1)) ->
-            case parseAndOrBeqOrLeqOrAeqOrNotOrTrueOrFalseOrPar restTokens1 of
+            case parseBeqOrNotOrAeqOrLeqOrTrueOrFalseOrPar restTokens1 of
                 Just (expr2, restTokens2) ->
                     Just (B_beq expr1 expr2, restTokens2)
                 Nothing -> Nothing
         result -> result
 
--- parseArithmeticExpr :: [Token] -> Maybe (Aexp, [Token])
--- parseArithmeticExpr tokens
---     = case parseSumOrSubOrProdOrIntOrVarOrPar tokens of
---         Just (expr, restTokens) -> Just (expr, restTokens)
---         _ -> Nothing
+-- And Beq Not Aeq Leq True False Par
+parseAndOrBeqOrNotOrAeqOrLeqOrTrueOrFalseOrPar :: [Token] -> Maybe (Bexp, [Token])
+parseAndOrBeqOrNotOrAeqOrLeqOrTrueOrFalseOrPar tokens
+    = case parseBeqOrNotOrAeqOrLeqOrTrueOrFalseOrPar tokens of 
+        Just (expr1, (T_and : restTokens1)) ->
+            case parseAndOrBeqOrNotOrAeqOrLeqOrTrueOrFalseOrPar restTokens1 of
+                Just (expr2, restTokens2) ->
+                    Just (B_and expr1 expr2, restTokens2)
+                Nothing -> Nothing
+        result -> result
